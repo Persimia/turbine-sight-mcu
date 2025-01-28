5caf5faa99cec25a73dbd476441315a9aa5ea5b7
Samuel Kemp <44552792+skemp117@users.noreply.github.com>
2025-01-28 12:02:25 -0500

    added led control

--- TurbineSightAdvanced/TurbineSightAdvanced/TurbineSightAdvanced.ino
+++ TurbineSightAdvanced/TurbineSightAdvanced/TurbineSightAdvanced.ino
@@ -3,21 +3,55 @@
 #include <unordered_map>
 
 // Pinout
-const int solenoid_control_H1_pin = 2;
-const int solenoid_control_H2_pin = 3;
-const int pump_control_H1_pin = 4;
-const int pump_control_H2_pin = 5; // need to check this
+const int pump_control_H1_pin = 2;
+const int solenoid_control_H1_pin = 3;
 const int AnalogPressurePin1 = 14;
 const int AnalogPressurePin2 = 15;
 
+const uint8_t numStrips = 2;
+const int LED_R[numStrips] = {9, 19};
+const int LED_G[numStrips] = {10, 18};
+const int LED_B[numStrips] = {11, 22};
+
 // Pressure related values
-const float Patm = 930.0;
-const float Pmin = 350.0;
+const float Patm = 970.0;
+const float Pmin = 360.0;
 const float attachPressureThreshold = 400.0;
 const float alpha = 0.1;
 float P1 = Patm;
 float P2 = Patm;
 bool pressureReporting = false;
+
+// LED Color table
+uint8_t colors[][3] = {
+    {255, 255, 255},  // White (Default) 0
+    {255, 0, 0},      // Red (Lass) 1
+    {255, 128, 0},    // Orange (LeadUp) 2
+    {255, 255, 0},    // Yellow (CoastIn) 3
+    {0, 255, 255},    // Cyan (WindDown) 4
+    {0, 0, 255},      // Blue (Vegetable) 5
+    {128, 0, 255},    // Violet (WindUp) 6
+    {255, 0, 255},    // Magenta (CoastOut) 7
+    {255, 0, 128},    // Rose (Recover) 8
+    {0, 0, 0},        // Off (all LEDs off) 9
+    {0, 255, 0},      // Green (CoastIn Suction) 10
+};
+
+enum class StateName : uint8_t {
+    Default = 0,
+    Lass, // 1
+    LeadUp, // 2
+    CoastIn, // 3
+    WindDown, // 4
+    Vegetable, // 5
+    WindUp, // 6
+    CoastOut, // 7
+    Recover, // 8
+    Exited, // 9
+    CoastInSuctionOn //10
+};
+
+StateName _lass_state_name = StateName::Exited;
 
 // MAVLink variables
 const uint8_t MAVLINK_SYS_ID = 1;
@@ -44,8 +78,59 @@ void handleAttach(uint8_t &value) {
   }
 }
 
-void handleLass(uint8_t &value) {
+void setColor(uint8_t strip, StateName state) {
+  uint8_t color = static_cast<uint8_t>(state);
+  analogWrite(LED_R[strip], colors[color][0]);
+  analogWrite(LED_G[strip], colors[color][1]);
+  analogWrite(LED_B[strip], colors[color][2]);
+}
 
+void handleLass(uint8_t &value) {
+  _lass_state_name = static_cast<StateName>(value);
+  switch (_lass_state_name){
+    case StateName::Default: // Default
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::Lass: // Lass
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::LeadUp: // LeadUp
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::CoastIn: // CoastIn
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::WindDown: // WindDown
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::Vegetable: // Vegetable
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::WindUp: // WindUp
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::CoastOut: // CoastOut
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::Recover: // Recover
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    case StateName::Exited: // Exited
+      setColor(0, _lass_state_name);
+      setColor(1, _lass_state_name);
+      break;
+    default:
+      break;
+  }
 }
 
 void handleTest(uint8_t &value) {
@@ -92,22 +177,22 @@ void initDetach() {
 
 void solenoidOn() { // releases vacuum
   digitalWrite(solenoid_control_H1_pin, HIGH);
-  digitalWrite(solenoid_control_H2_pin, LOW);
+  // digitalWrite(solenoid_control_H2_pin, LOW);
 }
 
 void solenoidOff() { // seals vacuum
   digitalWrite(solenoid_control_H1_pin, LOW);
-  digitalWrite(solenoid_control_H2_pin, LOW);
+  // digitalWrite(solenoid_control_H2_pin, LOW);
 }
 
 void pumpOn() {
   digitalWrite(pump_control_H1_pin, HIGH);
-  digitalWrite(pump_control_H2_pin, LOW);
+  // digitalWrite(pump_control_H2_pin, LOW);
 }
 
 void pumpOff() {
   digitalWrite(pump_control_H1_pin, LOW);
-  digitalWrite(pump_control_H2_pin, LOW);
+  // digitalWrite(pump_control_H2_pin, LOW);
 }
 
 void parseMavlinkInput() {
@@ -126,7 +211,7 @@ void parseMavlinkInput() {
           mavlink_msg_named_value_float_decode(&received_msg, &msg_struct);
           uint8_t value = (uint8_t)msg_struct.value;
           auto it = commandHandlers.find(msg_struct.name);
-          if (it != commandHandlers.end()) {it->second(value);} 
+          if (it != commandHandlers.end()) {it->second(value); sendNVF("ACK", value);} 
           else {} // unknown command
         }
         default:
@@ -146,11 +231,7 @@ void sendHeartbeat() {
     mavlink_msg_heartbeat_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, MAV_TYPE_GENERIC, MAV_AUTOPILOT_GENERIC, 0, 0, 0);
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
     Serial2.write(buf, len);
-    if (attachState) {
-      send_attached_msg();  // Send attach message via MAVLink
-    } else {
-      send_detached_msg();  // Send detach message via MAVLink
-    }
+    send_attached_msg();  // Send detach message via MAVLink
     if (pressureReporting) {
       reportPressure();
     }
@@ -158,32 +239,56 @@ void sendHeartbeat() {
   }
 }
 
-void send_attached_msg() {
-  mavlink_msg_named_value_float_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, millis(), "att_st", 1.0f);
+void sendNVF(const char* name, float value){
+  char name_bytes[10];
+  nullTerminateNVF(name, name_bytes, sizeof(name_bytes));
+  mavlink_msg_named_value_float_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, millis(), name_bytes, value);
   Serial2.write(buf, mavlink_msg_to_send_buffer(buf, &msg));
 }
 
-void send_detached_msg() {
-  mavlink_msg_named_value_float_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, millis(), "att_st", 0.0f);
-  Serial2.write(buf, mavlink_msg_to_send_buffer(buf, &msg));
+void nullTerminateNVF(const char* name, char* name_bytes, size_t size) {
+  memset(name_bytes, 0, size);  // Ensure all elements are initialized to '\0'
+  strncpy(name_bytes, name, size-1);  // Copy the string with null-termination
+}
+
+void send_attached_msg() {
+  sendNVF("att_st", (float)attachState);
 }
 
 void updatePressure(){
   attachState = false;
   P1 = alpha * analogRead(AnalogPressurePin1) + (1.0-alpha)*P1;
   P2 = alpha * analogRead(AnalogPressurePin2) + (1.0-alpha)*P2;
+  if ( _lass_state_name != StateName::CoastIn) {return;}
+  if (P1 < attachPressureThreshold) {
+    setColor(0, StateName::CoastInSuctionOn);
+  }
+  else {
+    setColor(0, StateName::CoastIn);
+  }
+  if (P2 < attachPressureThreshold) {
+    setColor(0, StateName::CoastInSuctionOn);
+  }
+  else {
+    setColor(0, StateName::CoastIn);
+  }
+  if (P1 < attachPressureThreshold && P2 < attachPressureThreshold) {
+    attachState = true;
+    send_attached_msg();
+  }
+  else {
+    attachState = false;
+    send_attached_msg();
+  }
 }
 
 void reportPressure(){
-  mavlink_msg_named_value_float_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, millis(), "suction1", P1);
-  Serial2.write(buf, mavlink_msg_to_send_buffer(buf, &msg));
-  mavlink_msg_named_value_float_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, millis(), "suction2", P2);
-  Serial2.write(buf, mavlink_msg_to_send_buffer(buf, &msg));
+  sendNVF("suction1", P1);
+  sendNVF("suction2", P2);
 }
 
 void ping() {
-  mavlink_msg_named_value_float_pack(MAVLINK_SYS_ID, MAVLINK_COMP_ID, &msg, millis(), "ping", 69.0f);
-  Serial2.write(buf, mavlink_msg_to_send_buffer(buf, &msg));
+  sendNVF("ping", 69.0f);
 }
 
 
@@ -193,9 +298,13 @@ void ping() {
 void setup() {
   // Setup switch input pin
   pinMode(solenoid_control_H1_pin,OUTPUT);
-  pinMode(solenoid_control_H2_pin, OUTPUT);
   pinMode(pump_control_H1_pin,OUTPUT);
-  pinMode(pump_control_H2_pin, OUTPUT);
+  for (int i = 0; i < numStrips; i++) {
+    pinMode(LED_R[i], OUTPUT);
+    pinMode(LED_G[i], OUTPUT);
+    pinMode(LED_B[i], OUTPUT);
+    setColor(i, _lass_state_name);
+  }
 
   pumpOff();
   solenoidOff();
